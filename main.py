11923fb84c27a7bc1d961a351036d7e86eeb361e
alina2002200 <alinasuper2002@mail.ru>
2020-12-15 16:01:29 +0300

    screen update

--- main.py
+++ main.py
@@ -3,46 +3,28 @@ from player_class import *
 from visual_module import *
 
 
-def check_display(list_obj, char):
-    o_high = list_obj[2]
-    o_low = list_obj[1]
-    o_left = list_obj[4]
-    o_right = list_obj[3]
-    if char.y - 600 > o_high.y:
-        for i in list_obj:
-            i.y += 600
-    if char.y + 1200 < o_low.y:
-        for i in list_obj:
-            i.y -= 600
-    if char.x - 800 > o_left.x:
-        for i in list_obj:
-            i.x += 800
-    if char.x + 1600 < o_right.x:
-        for i in list_obj:
-            i.x -= 800
-
 class Image():
     def __init__(self, x, y):
         self.x = x
         self.y = y
-        self.forest_surf = pygame.image.load('lol.jpg')
+        self.forest_surf = pygame.image.load('ok.jpg')
+        self.forest_surf = pygame.transform.scale(self.forest_surf,
+                                                  (2400, 1800))
         self.forest_rect = self.forest_surf.get_rect(
             bottomright=(self.x, self.y))
 
 
+def create_image(x, y, im_list):
+    image = Image(x + 800, y + 600)
+    im_list += [image]
+
+
 def gameplay(screen, clock, levelname):
-    image = Image(800, 600)
-    image_low = Image(800, 1200)
-    image_high = Image(800, 0)
-    image_right = Image(1600, 600)
-    image_left = Image(0, 600)
-    image_righth = Image(1600, 0)
-    image_lefth = Image(0, 0)
-    image_rightl = Image(1600, 1200)
-    image_leftl = Image(0, 1200)
-    image_list = [image, image_low, image_high,
-                  image_right, image_left, image_righth,
-                  image_lefth, image_rightl, image_leftl]
+    image_list = []
+    for i in range(-1, 3):
+        for j in range(-4, 2):
+            create_image(2400*i, 1800*j, image_list)
+
     FPS = 30
     finished = False
     player, level = level_read(levelname)
@@ -72,7 +54,6 @@ def gameplay(screen, clock, levelname):
         if apress:
             player.speedup(-1)
         player.move(image_list)
-        #check_display(image_list, player) I don't know where it has to be
         for obj in level:
             player.collision(obj, True)
         player.deathcheck()
